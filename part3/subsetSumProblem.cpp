e20a6ec5c58c8bdbd6a941783810b3dd58ea8be7
kanazawa <kaka_gegez@protonmail.com>
2025-08-06 00:10:58 +0900

    #13 部分和問題のプログラム

---
+++ part3/subsetSumProblem.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // 入力受取
+    int N, W;
+    cin >> N >> W;
+    vector<int> a(N);
+    for (int i = 0; i < N; i++)
+        cin >> a[i];
+    // bitは2^N通りの部分集合全体を動く
+
+    bool exist = false;
+    for (int bit = 0; bit < (1 << N); bit++)
+    {
+        int sum = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (bit & (1 << i))
+            {
+                sum += a[i];
+            }
+        }
+        // sumがWと一致するかどうか
+        if (sum == W)
+            exist = true;
+    }
+    if (exist)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
